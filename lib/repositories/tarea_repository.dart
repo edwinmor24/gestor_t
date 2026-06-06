133f3b6d48827c4b19387998dc614a6ed8a8d329
Edwinmor24 <hector2mor64@gmail.com>
2026-06-06 00:39:48 -0500

    Refactorizacion tarea_repository.dart

--- lib/repositories/tarea_repository.dart
+++ lib/repositories/tarea_repository.dart
@@ -6,14 +6,15 @@ class TareaRepository {
   // Ajusta esta URL según donde corras el emulador (ej: 'http://10.0.2.2:8000')
   final String baseUrl = 'http://127.0.0.1:8000';
 
-  // GET /api/
+  
+// GET /api/
   Future<List<Tarea>> obtenerTodas() async {
     final response = await http.get(Uri.parse('$baseUrl/api/'));
 
     if (response.statusCode == 200) {
-  List<dynamic> body = jsonDecode(utf8.decode(response.bodyBytes));
-  return body.map((dynamic item) => Tarea.fromJson(item)).toList();
-} else {
+      List<dynamic> body = jsonDecode(utf8.decode(response.bodyBytes));
+      return body.map((dynamic item) => Tarea.fromJson(item)).toList();
+    } else {
       throw Exception('Error al conectar con la Matrix de Django');
     }
   }
